07227f9ba565c9031143c08f9eec26484ff2e27c
romunov <romunov@gmail.com>
2017-10-02 21:34:16 +0200

    connects points in group, but doesn't remove them

--- server.R
+++ server.R
@@ -12,9 +12,14 @@ function(input, output, session) {
   })
   
   observe({
-    leafletProxy("map", data = filteredData()) %>%
+    outmap <- leafletProxy("map", data = filteredData()) %>%
       clearMarkers() %>%
       addCircleMarkers(lat = ~lat, lng = ~lng, radius = PS, weight = 1, color = "#777777", data = xy) %>%
       addCircleMarkers(lat = ~lat, lng = ~lng, radius = PS, weight = 1, color = "#2E86C1", fillOpacity = 0.7)
+    
+    for (i in unique(filteredData()$animal)) {
+      outmap <- addPolylines(map = outmap, lng = ~lng, lat = ~lat, data = filteredData()[filteredData()$animal == i, ])
+    }
+    outmap
   })
 }
