6b53be66f586791ac9b09d06ee63fc7c8b80a606
zankuralt <zan.kuralt@gmail.com>
2017-10-04 15:52:43 +0200

    Panel became interactive.

--- server.R
+++ server.R
@@ -1,76 +1,105 @@
 function(input, output, session) {
   
-  # Create map with no data, but do specify bounds
+  # Create empty map with specified bounds
   output$map <- renderLeaflet({
-    leaflet(xy) %>% 
-      addProviderTiles(providers$Stamen.Terrain,
-                       options = providerTileOptions(noWrap = TRUE, detectRetina = TRUE, reuseTiles = TRUE)) %>%
+    leaflet(data = xy) %>% 
+      addProviderTiles(provider = providers$Stamen.Terrain,
+                       options = providerTileOptions(noWrap = TRUE,
+                                                     detectRetina = TRUE,
+                                                     reuseTiles = TRUE)) %>% 
       fitBounds(~min(lng), ~min(lat), ~max(lng), ~max(lat))
   })
   
-  # add grey markers
-  observe({
-    leafletProxy(mapId = "map", data = xy) %>%
-      clearMarkers() %>% clearShapes() %>% 
-      addCircleMarkers(lat = ~lat, lng = ~lng, radius = PS, weight = 1,
-                       opacity = 0.1, data = xy, layerId = xy$id, fillColor = "black", 
-                       popup = paste(xy$type, "from", xy$animal, "on", xy$time, sep = " "))
+  # Subset data based on input from slider
+  fData <- reactive({
+    xy[(xy$animal %in% input$animals) & (xy$time >= input$datumrange[1] & xy$time <= input$datumrange[2]), ]
   })
   
-  # subset data based on input from slider and other input fields  
-  filteredData <- reactive({
-    xy[(xy$animal %in% input$animals) & (xy$time > min(input$datumrange) & xy$time < max(input$datumrange)), ]
-  })
+  # # Subset animal in within date range
+  # fDateRange <- reactive({
+  #   xy[(xy$time >= input$datumrange[1] & xy$time <= input$datumrange[2]), ]
+  # })
   
+  # Filter out siblings from data
   fSibs <- reactive({
-    ## Filter out siblings from all data
-    x <- filteredData()$animal
+    x <- fData()$animal
     sibs <- siblings[siblings$mother %in% x | siblings$father %in% x, "sibling"]
-    xy.sibs <- xy[(xy$animal %in% sibs) & (xy$time >= min(input$datumrange) & xy$time <= max(input$datumrange)), ] 
-    list(xy = xy.sibs, sibs = xy.sibs$animal)
+    xy[(xy$animal %in% sibs) & (xy$time >= input$datumrange[1] & xy$time <= input$datumrange[2]), ]
   })
   
+  # Add markers and lines for selected animals to map
   observe({
+    print(fData())
+    # Creates "baselayer"
+    outmap <- leafletProxy("map") %>% 
+      clearMarkers() %>%
+      clearShapes() %>%
+      addCircleMarkers(data = xy, lat = ~lat, lng = ~lng, radius = 7, stroke = FALSE, fill = TRUE,
+                       fillOpacity = 0.3, fillColor = "black", layerId = paste("allMarkers", xy$id, sep = " "),
+                       popup = paste(xy$type, "from", xy$animal, "on", xy$time, sep = " "))
     
-    # prime map with filtered data for animals
-    outmap <- leafletProxy(mapId = "map", data = filteredData()) 
-    
-    # add lines and points of selected animals
-    if (nrow(filteredData()) > 0) {
-      for (i in unique(filteredData()$animal)) {
-        outmap <- addPolylines(map = outmap, lng = ~lng, lat = ~lat,
-                               data = filteredData()[filteredData()$animal == i, ],
-                               color = "red", opacity = 0.7, weight = 4)
+    if (nrow(fData()) > 0) {
+      # Add lines
+      for (i in unique(fData()$animal)) {
+        print(i)
+        outmap <- addPolylines(map = outmap, lng = ~lng, lat = ~lat, 
+                               layerId = paste("aniLines", fData()$id[fData()$animal == i], sep = " "),
+                               data = fData()[fData()$animal == i, ],
+                               color = "red", opacity = 0.7, weight = 1)
       }
-      
-      # add points for animals
-      outmap <- outmap %>%
-        removeMarker(layerId = filteredData()$id) %>% # remove grey point before overplotting marker
-        addMarkers(lat = ~lat, lng = ~lng, icon = ~icons[filteredData()$type])
+      # Add markers
+      outmap %>%
+        removeMarker(layerId = paste("allMarkers", fData()$id, sep = " ")) %>% 
+        addCircleMarkers(data = fData(),
+                         lat = ~lat, lng = ~lng, radius = PS, stroke = FALSE,
+                         fillOpacity = 0.7, fillColor = "red", layerId = paste("aniMarkers", fData()$id, sep = " "),
+                         popup = paste(fData()$type, "from", fData()$animal, "on", fData()$time, sep = " "))
     }
-    
-    # Add lines and points of potential siblings
-    
-    if (nrow(fSibs()$xy) > 0) {
-      # Draws lines for siblings
-      for (i in unique(fSibs()$xy$animal)) {
-        outmap <- addPolylines(map = outmap, lng = ~lng, lat = ~lat,
-                               data = fSibs()$xy[fSibs()$xy$animal == i, ],
-                               color = "orange", opacity = 0.7, weight = 4)
-      }
-
-      # Draws points for siblings
-      outmap <- outmap %>%
-        removeMarker(layerId = fSibs()$xy$id) %>%
-        addMarkers(lat = ~lat, lng = ~lng, icon = ~icons[fSibs()$xy$type], data = fSibs()$xy[fSibs()$xy$animal %in% input$siblings, ])
-    }
-    
-    if (nrow(filteredData()) > 0) {
+  })
+  
+  output$animals <- renderUI({
+    selectInput("animals", "Select Animal", multiple = TRUE, choices = unique(xy$animal))
+  })
+  
+  
+  # If there is some input in input$animal, display this menu
+  observe({
+    if (nrow(fData()) == 0) {
+      output$siblings <- renderUI({ NULL })
+    } else {
       output$siblings <- renderUI({
-        selectInput("siblings", "Siblings", multiple = TRUE, choices = fSibs()$sibs)
+        selectInput("siblings", "Siblings", multiple = TRUE, choices = fSibs()$animal)
       })
     }
+  })
+  
+  # Add markers and lines for selected siblings to map
+  observe({
+    sibs <- input$siblings
+    sibs <- fSibs()[fSibs()$animal %in% sibs, ]
     
-    outmap
+    out.sibs <- leafletProxy("map") %>% 
+      # Remove all previous points and lines
+      clearGroup(group = "sibMarkers") %>% 
+      clearGroup(group = "sibLines")
+    
+    if (nrow(sibs) > 0) {
+      out.sibs <- leafletProxy("map") %>% 
+        # Remove all previous points and lines
+        clearGroup(group = "sibMarkers") %>% 
+        clearGroup(group = "sibLines")
+      # Add lines
+      for (i in unique(sibs$animal)) {
+        out.sibs <- addPolylines(map = out.sibs, lng = ~lng, lat = ~lat, 
+                                 layerId = paste("sibLines", sibs$id[sibs$animal == i], sep = " "),
+                                 data = sibs[sibs$animal == i, ],
+                                 color = "orange", opacity = 0.7, weight = 4, group = "sibLines")
+      }
+      # Add points
+      out.sibs %>%
+        addMarkers(lat = ~lat, lng = ~lng, icon = ~icons[sibs$type], 
+                   layerId = paste("sibMarkers", sibs$id, sep = " "),
+                   data = sibs, group = "sibMarkers")
+    }
   })
 }
--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ bootstrapPage(
   absolutePanel(top = 10, right = 10,
                 sliderInput("datumrange", "Datum range", min(xy$time), max(xy$time),
                             value = range(xy$time), step = 1),
-                selectInput("animals", "Select Animal", multiple = TRUE, choices = unique(xy$animal), selected = "001"),
+                uiOutput("animals"),
                 uiOutput("siblings")
   )
 )
