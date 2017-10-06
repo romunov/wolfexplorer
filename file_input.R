8e99690ae1a98481405e469852bd3dfd656fc0eb
zankuralt <zan.kuralt@gmail.com>
2017-10-06 12:24:50 +0200

    GKtoWGS implemented in file_input

--- file_input.R
+++ file_input.R
@@ -3,9 +3,11 @@ inputFileSamples <- reactive({
   if (is.null(x)) {
     data.frame(lng = NA, lat = NA, date = NA, sample_type = NA, animal = NA, sex = NA, sample_name = NA, id = NA)[0, ]
   } else {
-    read.csv(x$datapath, header = TRUE, sep = ",",
-             encoding = "UTF-8", stringsAsFactors = FALSE, 
-             colClasses = c("numeric", "numeric", "Date", "character", "character", "character", "character" ))
+    x <- read.csv(x$datapath, header = TRUE, sep = ",",
+                  encoding = "UTF-8", stringsAsFactors = FALSE, 
+                  colClasses = c("numeric", "numeric", "Date", "character", "character", "character", "character" ))
+    x <- GKtoWGS(x)
+    x
   }
 })
 
