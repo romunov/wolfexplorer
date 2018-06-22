b6e3a63adc0998a33c43c3e173581ea323fa4b2d
zankuralt <zan.kuralt@gmail.com>
2018-06-22 14:19:51 +0200

    rotated labels of pedigree plot for 90 degrees

--- R/plot_pedigree.R
+++ R/plot_pedigree.R
@@ -19,7 +19,7 @@ observe({
                      missid = "0")
     
     output$pedigree.plot <- renderPlot({
-      plot(pdgr, cex = 0.65)
+      plot(pdgr, cex = 1, srt = 90)
     })
     
     # this answer helped with collapsible panel - https://stackoverflow.com/questions/35175167/collapse-absolutepanel-in-shiny/35175847
